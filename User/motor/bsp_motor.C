8168841195de262347b704adcbbf4b6690443f43
hhhhhazd <2504302531@qq.com>
2025-03-03 22:36:59 +0800

    v4.27

--- User/motor/bsp_motor.C
+++ User/motor/bsp_motor.C
@@ -47,7 +47,7 @@ int cw_5, cw_2, cw_3, cw_4;
 // 微调坐标
 // gg=0就是色环,gg1就是原料区的色块,gg=2就是地上色块，gg=3就是色块上叠加色块
 #define gg_0_X 153
-#define gg_0_Y 105
+#define gg_0_Y 103
 #define gg_1_X 145
 #define gg_1_Y 108
 #define gg_2_X 150
